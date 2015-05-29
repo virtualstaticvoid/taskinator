6842f4a17008599c2becc6701711ec1616cbecd6
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2015-05-29 10:16:57 +0200

    removed unused code

--- lib/taskinator/logger.rb
+++ lib/taskinator/logger.rb
@@ -5,11 +5,6 @@
 # the LGPLv3 license.  Please see <http://www.gnu.org/licenses/lgpl-3.0.html>
 # for license text.
 #
-# Sidekiq Pro has a commercial-friendly license allowing private forks
-# and modifications of Sidekiq.  Please see http://sidekiq.org/pro/ for
-# more detail.  You can find the commercial license terms in COMM-LICENSE.
-#
-
 require 'time'
 require 'logger'
 
@@ -28,82 +23,25 @@ module Taskinator
       end
     end
 
-    def self.with_context(msg)
-      begin
-        Thread.current[:taskinator_context] = msg
-        yield
-      ensure
-        Thread.current[:taskinator_context] = nil
-      end
-    end
+    class << self
 
-    def self.initialize_logger(log_target = STDOUT)
-      oldlogger = defined?(@logger) ? @logger : nil
-      @logger = Logger.new(log_target)
-      @logger.level = Logger::INFO
-      @logger.formatter = Pretty.new
-      oldlogger.close if oldlogger && !$TESTING # don't want to close testing's STDOUT logging
-      @logger
-    end
-
-    def self.logger
-      defined?(@logger) ? @logger : initialize_logger
-    end
-
-    def self.logger=(log)
-      @logger = (log ? log : Logger.new('/dev/null'))
-    end
-
-    # This reopens ALL logfiles in the process that have been rotated
-    # using logrotate(8) (without copytruncate) or similar tools.
-    # A +File+ object is considered for reopening if it is:
-    #   1) opened with the O_APPEND and O_WRONLY flags
-    #   2) the current open file handle does not match its original open path
-    #   3) unbuffered (as far as userspace buffering goes, not O_SYNC)
-    # Returns the number of files reopened
-    def self.reopen_logs
-      to_reopen = []
-      append_flags = File::WRONLY | File::APPEND
-
-      ObjectSpace.each_object(File) do |fp|
-        begin
-          if !fp.closed? && fp.stat.file? && fp.sync && (fp.fcntl(Fcntl::F_GETFL) & append_flags) == append_flags
-            to_reopen << fp
-          end
-        rescue IOError, Errno::EBADF
-        end
+      def initialize_logger(log_target = STDOUT)
+        oldlogger = defined?(@logger) ? @logger : nil
+        @logger = Logger.new(log_target)
+        @logger.level = Logger::INFO
+        @logger.formatter = Pretty.new
+        oldlogger.close if oldlogger && !$TESTING # don't want to close testing's STDOUT logging
+        @logger
       end
 
-      nr = 0
-      to_reopen.each do |fp|
-        orig_st = begin
-          fp.stat
-        rescue IOError, Errno::EBADF
-          next
-        end
-
-        begin
-          b = File.stat(fp.path)
-          next if orig_st.ino == b.ino && orig_st.dev == b.dev
-        rescue Errno::ENOENT
-        end
-
-        begin
-          File.open(fp.path, 'a') { |tmpfp| fp.reopen(tmpfp) }
-          fp.sync = true
-          nr += 1
-        rescue IOError, Errno::EBADF
-          # not much we can do...
-        end
+      def logger
+        defined?(@logger) ? @logger : initialize_logger
       end
-      nr
-    rescue RuntimeError => ex
-      # RuntimeError: ObjectSpace is disabled; each_object will only work with Class, pass -X+O to enable
-      puts "Unable to reopen logs: #{ex.message}"
-    end
 
-    def logger
-      Taskinator::Logging.logger
+      def logger=(log)
+        @logger = (log ? log : Logger.new('/dev/null'))
+      end
+
     end
   end
 end
