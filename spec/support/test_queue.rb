278a71622d03e2c43631cc63646f2c1ad58c8de8
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2015-07-28 13:14:02 +0200

    added new queue adapter for testingm, which executes immediately

--- spec/support/test_queue.rb
+++ spec/support/test_queue.rb
@@ -5,6 +5,10 @@ module Taskinator
       TestQueueAdapter.new
     end
 
+    def self.create_test_queue_worker_adapter(config={})
+      QueueWorkerAdapter.new
+    end
+
     class TestQueueAdapter
 
       attr_reader :creates
@@ -34,5 +38,25 @@ module Taskinator
       end
 
     end
+
+    #
+    # this is a synchronous implementation for use in testing
+    #
+    class QueueWorkerAdapter
+
+      def enqueue_create_process(definition, uuid, args)
+        Taskinator::CreateProcessWorker.new(definition.name, uuid, args).perform
+      end
+
+      def enqueue_task(task)
+        Taskinator::TaskWorker.new(task.uuid).perform
+      end
+
+      def enqueue_job(job)
+        Taskinator::JobWorker.new(job.uuid).perform
+      end
+
+    end
+
   end
 end
