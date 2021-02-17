65b5d9cc3c3af38e3c10b887a2066d191473f0de
Chris Stefano <chris.stefano@statpro.com>
2021-02-17 16:31:18 +0000

    bug fix pending task counts

--- lib/taskinator/process.rb
+++ lib/taskinator/process.rb
@@ -199,6 +199,12 @@ module Taskinator
       end
 
       def task_completed(task)
+        # deincrement the count of pending sequential tasks
+        pending = deincr_pending_tasks
+
+        Taskinator.statsd_client.count("taskinator.#{definition.name.underscore.parameterize}.pending", pending)
+        Taskinator.logger.info("Completed task for process '#{uuid}'. Pending is #{pending}.")
+
         next_task = task.next
         if next_task
           next_task.enqueue!
